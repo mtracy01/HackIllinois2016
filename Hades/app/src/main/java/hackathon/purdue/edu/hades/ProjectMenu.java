76570a1907625bdbe133a0478d8c0692ef4221d7
mtracy01 <matthetracy@gmail.com>
2016-02-19 23:31:50 -0600

    Added ProjectMenu Activity

---
+++ Hades/app/src/main/java/hackathon/purdue/edu/hades/ProjectMenu.java
@@ -0,0 +1,13 @@
+package hackathon.purdue.edu.hades;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjectMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_menu);
+    }
+}
